2a2e7c6960d7784c37c23ee989032eff41c3eac5
me <measateacher@gmail.com>
2024-09-16 16:27:08 +0300

    Lecture 2

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
 from flask import Flask, render_template
 
+# app = Flask(__name__, template_folder='')
 app = Flask(__name__)
+
+pets = [
+    {
+        'species':'cat',
+        'name':'Tom',
+        'age':10,
+        'color':'grey'
+    },
+    {
+        'species':'cat',
+        'name':'Vasko',
+        'age':5,
+        'color':'white'
+    },
+    {
+        'species':'dog',
+        'name':'Patron',
+        'age':7,
+        'color':'brown'
+    },
+]
+
+@app.route("/pets/")
+def our_pets():
+    return render_template('pets.jinja', 
+                           pets=pets,
+                           title="Our Pets"
+                        )              
+
 
 @app.route("/")
 def hello_world():
+    name, age, profession = "Mark", 19, 'Programmer'
+    template_context = dict(name=name, age=age, profession=profession)
     # return "<h1>Hello, NTU 'Khpi'!</h1>"
-    return render_template('index.html')
+    return render_template('index.jinja', 
+                        #    name=name,
+                        #    age=age,
+                        #    profession=profession
+                        **template_context
+                           )
 
 @app.route("/about/")
 def about():
